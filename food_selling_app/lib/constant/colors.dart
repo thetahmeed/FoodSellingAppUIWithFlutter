7da2bb2d54591e6325480a1b72dee710e582f965
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-10-17 13:17:45 +0600

    Colors Added

---
+++ food_selling_app/lib/constant/colors.dart
@@ -0,0 +1,5 @@
+const String primaryColor = '#EF7822';
+const String secondaryColor = '#B5B5B5';
+const String backgroundColor = '#FEFEFE';
+const String fontColor = '#000743';
+const String offTagColor = '#00CF46';
